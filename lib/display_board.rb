94578d3d1a74255f784c6c257938cbbbd845e6aa
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-06 14:14:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts  "board = [#{" "," "," "},#{" "," "," "},#{" "," "," "}]"
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+  puts  "board = [" "," "," "," "," "," "," "," "," "]"
   puts " the first row is #{[0]}"
   puts " the second row is #{[2]}"
   puts " the third row is #{[4]}"
